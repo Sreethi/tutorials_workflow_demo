c9005baf7d3505b09a07b0d89523c815aa9a4df8
“Anjana <anjanaprasad.g@gmail.com>
2021-11-13 09:37:12 -0800

    first commit

---
+++ tutorials/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from django.urls import reverse
+import pytest
+
+
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+
+# Create your tests here.
